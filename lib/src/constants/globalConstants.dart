5ea9b12191220f9f31e9d01ca6173f2d0ab9381e
Ironmar <fmrm93@gmail.com>
2020-10-31 22:42:42 -0500

    RA-8 add background and title

---
+++ lib/src/constants/globalConstants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Size screenSize(BuildContext context) => MediaQuery.of(context).size;
+
+Color primaryColor(BuildContext context) => Theme.of(context).primaryColor;
+
+Color secondaryColor(BuildContext context) => Theme.of(context).accentColor;
+
+Color darkPrimary(BuildContext context) => Color(0xFF0C475D);
+
+Color gray(BuildContext context) => Color(0x5D5D5D);
+
+TextStyle title(BuildContext context) => Theme.of(context).textTheme.headline1;
+
+TextStyle subtitle(BuildContext context) =>
+    Theme.of(context).textTheme.headline2;
+
+TextStyle bodyText(BuildContext context) =>
+    Theme.of(context).textTheme.bodyText1;
--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:recuerditos/src/constants/globalConstants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo'),
+      body: Stack(
+        children: [
+          background(),
+          body(context),
+        ],
       ),
     );
   }
+
+  Widget background() => Center(
+        child: SvgPicture.asset(
+          'assets/irregularShape.svg',
+          fit: BoxFit.cover,
+        ),
+      );
+  Widget body(BuildContext context) => Container(
+        padding: EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+        child: Column(
+          children: [
+            Text(
+              'Recuerditos',
+              style: title(context),
+            ),
+            Row(children: [
+              Text(
+                'Diviertete aprendiendo',
+                style: subtitle(context),
+                textAlign: TextAlign.start,
+              ),
+            ])
+          ],
+        ),
+      );
 }
